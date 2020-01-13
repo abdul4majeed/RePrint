2b948b90f62ee8e0187c80041b66f5c052ee4edb
AbdulMajeed <abdul4majeed@gmail.com>
2020-01-13 11:04:09 +0500

    implement the payment gateway

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,19 @@ Route::resource('shops','ShopController');
 
 
 Route::post('order',function(Request $req){
-    dd($req->all());
+    if($req->payment == "myfatorah")
+    {
+        return redirect()->route('fatroorah-payment');
+    }
+    elseif($req->payment == "tap")
+    {
+        return redirect()->route('tap-payment');
+    }
+    elseif($req->payment == "cash_on_delivery")
+    {
+        return "cash on delivery save record in db and continue";
+    }
+    // dd($req->all());
 })->name('order');
 
 
@@ -163,3 +175,212 @@ Route::get('verify_account/{email}/{token}',function($email,$token ){
 
 })->name('verify_account');
 
+
+Route::get('tap-payment',function(){
+    return view('payment');
+})->name('tap-payment');
+
+Route::post('charge',function(Request $req){
+
+    $ammount = 1;
+    $token = $req->tapToken;
+    $currency = "KWD";
+    $threeDSecure = "true";
+    $save_card = "false";
+    $description = "Test Description";
+    $statement_descriptor = "Sample";
+    $metadata_one ="test 1";
+    $metadata_two ="test 2";
+    $reference_transaction = "txn_0001";
+    $reference_order = "ord_0001";
+    $receipt_email  = "false";
+    $receipt_sms  = "true";
+    $customer_first_name = "first name";
+    $customer_middle_name = "middle name";
+    $customer_last_name = "last name";
+    $customer_email = "test@test.com";
+    $customer_phone_code = "965";
+    $customer_phone_number = "50000000";
+    $post_url = route('post_url');
+    $redirect_url = "http://127.0.0.1:8000/redirect_url";
+    $json = "{\"amount\":$ammount,\"currency\":\"$currency\",\"threeDSecure\":$threeDSecure,\"save_card\":$save_card,\"description\":\"$description\",\"statement_descriptor\":\"$statement_descriptor\",\"metadata\":{\"udf1\":\"$metadata_one\",\"udf2\":\"$metadata_two\"},\"reference\":{\"transaction\":\"$reference_transaction\",\"order\":\"$reference_order\"},\"receipt\":{\"email\":$receipt_email,\"sms\":$receipt_sms},\"customer\":{\"first_name\":\"$customer_first_name\",\"middle_name\":\"$customer_middle_name\",\"last_name\":\"$customer_last_name\",\"email\":\"$customer_email\",\"phone\":{\"country_code\":\"$customer_phone_code\",\"number\":\"$customer_phone_number\"}},\"source\":{\"id\":\"$token\"},\"post\":{\"url\":\"$post_url\"},\"redirect\":{\"url\":\"$redirect_url\"}}";
+    $curl = curl_init();
+    // dd($json);
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.tap.company/v2/charges",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $json,
+      CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer sk_test_XKokBfNWv6FIYuTMg5sLPjhJ",
+        "content-type: application/json"
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+    
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+        $decoded = json_decode($response);
+        if($decoded->status == "INITIATED")
+        {
+            return redirect()->to($decoded->transaction->url);
+        }
+    }
+})->name('charge');
+
+Route::post('post_url',function(Request $req){
+    dd($req->all());
+})->name('post_url');
+
+
+Route::get('/redirect_url',function(Request $req){
+    $id = $req->tap_id;
+    $curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.tap.company/v2/charges/$id",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "{}",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: Bearer sk_test_XKokBfNWv6FIYuTMg5sLPjhJ"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  dd($response);
+}
+})->name('redirect_url');
+
+
+// MyFatrooh
+
+Route::get('myfatroohah',function(){
+    ####### Initiate Payment ######
+$token = "7Fs7eBv21F5xAocdPvvJ-sCqEyNHq4cygJrQUFvFiWEexBUPs4AkeLQxH4pzsUrY3Rays7GVA6SojFCz2DMLXSJVqk8NG-plK-cZJetwWjgwLPub_9tQQohWLgJ0q2invJ5C5Imt2ket_-JAlBYLLcnqp_WmOfZkBEWuURsBVirpNQecvpedgeCx4VaFae4qWDI_uKRV1829KCBEH84u6LYUxh8W_BYqkzXJYt99OlHTXHegd91PLT-tawBwuIly46nwbAs5Nt7HFOozxkyPp8BW9URlQW1fE4R_40BXzEuVkzK3WAOdpR92IkV94K_rDZCPltGSvWXtqJbnCpUB6iUIn1V-Ki15FAwh_nsfSmt_NQZ3rQuvyQ9B3yLCQ1ZO_MGSYDYVO26dyXbElspKxQwuNRot9hi3FIbXylV3iN40-nCPH4YQzKjo5p_fuaKhvRh7H8oFjRXtPtLQQUIDxk-jMbOp7gXIsdz02DrCfQIihT4evZuWA6YShl6g8fnAqCy8qRBf_eLDnA9w-nBh4Bq53b1kdhnExz0CMyUjQ43UO3uhMkBomJTXbmfAAHP8dZZao6W8a34OktNQmPTbOHXrtxf6DS-oKOu3l79uX_ihbL8ELT40VjIW3MJeZ_-auCPOjpE3Ax4dzUkSDLCljitmzMagH2X8jN8-AYLl46KcfkBV"; #token value to be placed here;
+$basURL = "https://apitest.myfatoorah.com";
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "$basURL/v2/InitiatePayment",
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\"InvoiceAmount\": 1,\"CurrencyIso\": \"KWD\"}",
+  CURLOPT_HTTPHEADER => array("Authorization: Bearer $token","Content-Type: application/json"),
+));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+    $decodeResponse = json_decode($response);
+    if($decodeResponse->IsSuccess)
+    {
+        return view('fatrah')->with('cards',$decodeResponse->Data->PaymentMethods);
+    }
+}
+})->name('fatroorah-payment');
+
+Route::post('exe_fatroah',function(Request $req){
+
+    
+    $paymentId = $req->card_id;
+    $callBackUrl = "http://www.example.com";// route('fatrooh_redirect');
+    $errorUrl = "http://www.test.com" ; //route('fatrooh_error');
+    ####### Execute Payment ######
+    $token = "7Fs7eBv21F5xAocdPvvJ-sCqEyNHq4cygJrQUFvFiWEexBUPs4AkeLQxH4pzsUrY3Rays7GVA6SojFCz2DMLXSJVqk8NG-plK-cZJetwWjgwLPub_9tQQohWLgJ0q2invJ5C5Imt2ket_-JAlBYLLcnqp_WmOfZkBEWuURsBVirpNQecvpedgeCx4VaFae4qWDI_uKRV1829KCBEH84u6LYUxh8W_BYqkzXJYt99OlHTXHegd91PLT-tawBwuIly46nwbAs5Nt7HFOozxkyPp8BW9URlQW1fE4R_40BXzEuVkzK3WAOdpR92IkV94K_rDZCPltGSvWXtqJbnCpUB6iUIn1V-Ki15FAwh_nsfSmt_NQZ3rQuvyQ9B3yLCQ1ZO_MGSYDYVO26dyXbElspKxQwuNRot9hi3FIbXylV3iN40-nCPH4YQzKjo5p_fuaKhvRh7H8oFjRXtPtLQQUIDxk-jMbOp7gXIsdz02DrCfQIihT4evZuWA6YShl6g8fnAqCy8qRBf_eLDnA9w-nBh4Bq53b1kdhnExz0CMyUjQ43UO3uhMkBomJTXbmfAAHP8dZZao6W8a34OktNQmPTbOHXrtxf6DS-oKOu3l79uX_ihbL8ELT40VjIW3MJeZ_-auCPOjpE3Ax4dzUkSDLCljitmzMagH2X8jN8-AYLl46KcfkBV"; #token value to be placed here;
+
+    $basURL = "https://apitest.myfatoorah.com";
+
+    $object =  "{\"PaymentMethodId\":\"$paymentId\",\"CustomerName\": \"Ahmed\",\"DisplayCurrencyIso\": \"KWD\", \"MobileCountryCode\":\"+965\",\"CustomerMobile\": \"92249038\",\"CustomerEmail\": \"aramadan@myfatoorah.com\",\"InvoiceValue\": 2,\"CallBackUrl\": \"$callBackUrl\",\"ErrorUrl\": \"$errorUrl\",\"Language\": \"en\",\"CustomerReference\" :\"ref 1\",\"CustomerCivilId\":12345678,\"UserDefinedField\": \"Custom field\",\"ExpireDate\": \"\",\"CustomerAddress\" :{\"Block\":\"\",\"Street\":\"\",\"HouseBuildingNo\":\"\",\"Address\":\"\",\"AddressInstructions\":\"\"},\"InvoiceItems\": [{\"ItemName\": \"Product 01\",\"Quantity\": 1,\"UnitPrice\": 2}]}";
+  
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "$basURL/v2/ExecutePayment",
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => $object,
+  CURLOPT_HTTPHEADER => array("Authorization: Bearer $token","Content-Type: application/json"),
+));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+    $decodeResponse = json_decode($response);
+    if($decodeResponse->IsSuccess)
+    {
+        return redirect()->to($decodeResponse->Data->PaymentURL);
+    }
+
+}
+})->name('exe_fatrah');
+
+Route::get('fatrah/redirect',function(Request $req){
+        $key = $req->paymentId;
+        $keyType = "PaymentId";
+       
+
+        ####### Get Payment Detail ######
+        $token = "7Fs7eBv21F5xAocdPvvJ-sCqEyNHq4cygJrQUFvFiWEexBUPs4AkeLQxH4pzsUrY3Rays7GVA6SojFCz2DMLXSJVqk8NG-plK-cZJetwWjgwLPub_9tQQohWLgJ0q2invJ5C5Imt2ket_-JAlBYLLcnqp_WmOfZkBEWuURsBVirpNQecvpedgeCx4VaFae4qWDI_uKRV1829KCBEH84u6LYUxh8W_BYqkzXJYt99OlHTXHegd91PLT-tawBwuIly46nwbAs5Nt7HFOozxkyPp8BW9URlQW1fE4R_40BXzEuVkzK3WAOdpR92IkV94K_rDZCPltGSvWXtqJbnCpUB6iUIn1V-Ki15FAwh_nsfSmt_NQZ3rQuvyQ9B3yLCQ1ZO_MGSYDYVO26dyXbElspKxQwuNRot9hi3FIbXylV3iN40-nCPH4YQzKjo5p_fuaKhvRh7H8oFjRXtPtLQQUIDxk-jMbOp7gXIsdz02DrCfQIihT4evZuWA6YShl6g8fnAqCy8qRBf_eLDnA9w-nBh4Bq53b1kdhnExz0CMyUjQ43UO3uhMkBomJTXbmfAAHP8dZZao6W8a34OktNQmPTbOHXrtxf6DS-oKOu3l79uX_ihbL8ELT40VjIW3MJeZ_-auCPOjpE3Ax4dzUkSDLCljitmzMagH2X8jN8-AYLl46KcfkBV"; #token value to be placed here;
+    
+        $basURL = "https://apitest.myfatoorah.com";
+    
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "$basURL/v2/GetPaymentStatus",
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => "{\"Key\":\"$key\",\"KeyType\": \"$keyType\"}",
+      CURLOPT_HTTPHEADER => array("Authorization: Bearer $token","Content-Type: application/json"),
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+    
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+        $decodeResponse = json_decode($response);
+        dd($decodeResponse);
+        if($decodeResponse->IsSuccess)
+        {
+            return redirect()->to($decodeResponse->Data->PaymentURL);
+        }
+    
+    }
+})->name('fatrooh_redirect');
+
+
+Route::get('fatrooh_error',function(Request $req){
+    dd($req->all());
+})->name('fatrooh_error');
